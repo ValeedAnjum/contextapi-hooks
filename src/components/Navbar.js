59804289291d06bdb87cacfa852e7156ff92dd2b
ValeedAnjum <allinone644@gmail.com>
2020-06-03 10:39:39 +0500

    Accessing Context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-
+import { ThemeContext } from '../contexts/ThemeContext';
 class Navbar extends Component {
+  static contextType = ThemeContext;
   render() { 
+    console.log(this.context);
     return ( 
       <nav>
         <h1>Context App</h1>
